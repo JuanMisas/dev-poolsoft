947a7b65eac95f12c994323293b2b3a4e3aa8afa
bmisas2015 <bmisas123@gmail.com>
2021-03-18 00:18:10 -0400

    Version 1

--- controllers/PoolTypeController.js
+++ controllers/PoolTypeController.js
@@ -4,8 +4,29 @@ const server = require('../server/server');
 
 module.exports = {
     
-    async findPoolType(id) {
+    async findPoolTypeOne(id) {
         const PoolType = await PT.findByPk(id);
         return PoolType;
-    }
+    },
+
+    async findAllPoolType(){
+        const PoolType = await PT.findAll({where : {}});
+        return PoolType;
+    },
+
+    async CretePoolType(NewPooltype) {
+          const PoolType = await PT.create(NewPooltype);
+          return PoolType;
+    },
+
+    async UpdatePoolType(body, id) {
+        const PoolType = await PT.update(body , {where : { idTypePool : id}});
+        return PoolType;
+    },
+
+    async DeletePoolType(id) {
+        const PoolType = await PT.destroy({where : { idTypePool : id}});
+        return PoolType;
+    },
+
 };
--- server/routes/Pooltype.js
+++ server/routes/Pooltype.js
@@ -1,14 +1,31 @@
 const routes = require('express').Router();
 
 const PoolType = require('../../models/ps_pooltype');
-
-const PoolTypeController = require('../../controllers/poolTypeController')
+const PoolTypeController = require('../../controllers/PoolTypeController');
 
 routes.get('/PoolType/:id', async (req, res) => {
-   
-    pt = await PoolTypeController.findPoolType(req.params.id) ;
-    console.log(pt);
+    pt = await PoolTypeController.findPoolTypeOne(req.params.id) ;
     res.json(pt);
+});
+
+routes.get('/PoolType/', async (req, res) => {
+    pt = await PoolTypeController.findAllPoolType();
+    res.json(pt);
+});
+
+routes.post('/Pooltype/', async (req, res) => {
+    pt = await PoolTypeController.CretePoolType(req.body);
+    res.json(pt); 
+});
+
+routes.put('/Pooltype/:id', async (req, res) => {
+    pt = await PoolTypeController.UpdatePoolType(req.body, req.params.id);
+    res.json(pt); 
+});
+
+routes.delete('/Pooltype/:id', async (req, res) => {
+    pt = await PoolTypeController.DeletePoolType(req.params.id);
+    res.json(pt); 
 });
 
 module.exports = routes;
